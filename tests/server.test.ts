d19a7dd3402aeff1fed4649628e693f4fa13ff61
developedyrod <vitalidade.08-devorador@icloud.com>
2024-06-01 12:42:08 -0300

    chore: configuring jest

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -2,10 +2,19 @@ import request from "supertest"
 
 import app from "../src/shared/http/server"
 
+let server: any
+beforeAll(() => {
+  server = app.listen(3000)
+})
+
 describe("GET /status", () => {
   it("Should return a status code of 200", async () => {
     const response = await request(app).get("/status")
     expect(response.status).toBe(200)
     expect(response.body).toEqual({ message: "Server is running on port 3000" })
   })
+})
+
+afterAll((done) => {
+  server.close(done)
 })
